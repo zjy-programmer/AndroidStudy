46606c75db2c4d3f58179ccb90fb9ac8dce6b637
zjy <zjy_programmer@163.com>
2021-10-25 15:07:11 +0800

    11章仪表盘作业第一版提交

--- app/src/main/java/com/example/androidstudy/hencoderplus/chapter11/DashBoardView.kt
+++ app/src/main/java/com/example/androidstudy/hencoderplus/chapter11/DashBoardView.kt
@@ -1,10 +1,7 @@
 package com.example.androidstudy.hencoderplus.chapter11
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.DashPathEffect
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -19,22 +16,46 @@ import android.view.View
 class DashBoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private var centerX = 0f
     private var centerY = 0f
-    private val dashBoardRadius = 200f
+    private var dashBoardRadius = 400f
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val path = Path()
+    private val pathMeasure = PathMeasure()
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         centerX = (w / 2).toFloat()
         centerY = (h / 2).toFloat()
-        val floats = floatArrayOf(10f, 20f)
+        dashBoardRadius = centerX - 50
+
+        path.addArc(centerX - dashBoardRadius,
+            centerY - dashBoardRadius,
+            centerX + dashBoardRadius,
+            centerY + dashBoardRadius,
+            150f,
+            250f)
+
+        pathMeasure.setPath(path, false)
+        val floats = floatArrayOf(10f, (pathMeasure.length - 21 * 10f) / 20)
         paint.pathEffect = DashPathEffect(floats, 0f)
         paint.color = Color.BLACK
         paint.style = Paint.Style.STROKE
-        paint.strokeWidth = 10f
+        paint.strokeWidth = 40f
+
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawCircle(centerX, centerY, dashBoardRadius, paint)
+        canvas.drawPath(path, paint)
+        paint.pathEffect = null
+        paint.strokeWidth = 5f
+        canvas.drawArc(centerX - dashBoardRadius - 20,
+            centerY - dashBoardRadius - 20,
+            centerX + dashBoardRadius + 20,
+            centerY + dashBoardRadius + 20,
+            150f,
+            250f,
+            false,
+            paint)
+        canvas.drawLine(centerX, centerY, centerX - 300, centerY - 60, paint)
     }
 }
