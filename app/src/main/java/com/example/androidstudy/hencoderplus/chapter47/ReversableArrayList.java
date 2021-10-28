ea99d4e8a57a442f747e3dd8940e6456f55e9b2f
zjy <zjy_programmer@163.com>
2021-10-28 18:49:16 +0800

    47章第3个作业第一版提交

---
+++ app/src/main/java/com/example/androidstudy/hencoderplus/chapter47/ReversableArrayList.java
@@ -0,0 +1,14 @@
+package com.example.androidstudy.hencoderplus.chapter47;
+
+import java.util.ArrayList;
+
+/**
+ * ProjectName: AndroidStudy
+ * Package: com.example.androidstudy.hencoderplus.chapter47
+ * ClassName: ReversableArrayList
+ * CreateDate: 2021/10/28 6:48 下午
+ * Author: zjy
+ * Description:这个类会抹掉泛型信息，要求：让ReversableArrayList保留泛型信息
+ */
+public class ReversableArrayList<T> extends ArrayList<T> {
+}
