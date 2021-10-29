d3111e63516b898e007a9ad1cc1fd3383cee47d8
zjy <zjy_programmer@163.com>
2021-10-29 12:08:49 +0800

    54章作业第一版提交

---
+++ app/src/main/java/com/example/androidstudy/hencoderplus/chapter54/Test54.kt
@@ -0,0 +1,17 @@
+package com.example.androidstudy.hencoderplus.chapter54
+
+/**
+ * ProjectName: AndroidStudy
+ * Package: com.example.androidstudy.hencoderplus.chapter54
+ * ClassName: Test54
+ * CreateDate: 2021/10/29 12:06 下午
+ * Author: zjy
+ * Description: 用Kotlin写一个泛型类型和一个泛型接口
+ */
+class Test54<T> {
+
+}
+
+interface Test54Interface<E> {
+
+}
