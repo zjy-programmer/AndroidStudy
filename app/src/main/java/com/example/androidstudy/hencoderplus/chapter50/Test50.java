3084530f9bbed6d2bc77685b165a22a6ce4eebfa
zjy <zjy_programmer@163.com>
2021-10-29 11:57:13 +0800

    50章第2个作业第二版提交 题目在第二页 不过跟我猜的一样

--- app/src/main/java/com/example/androidstudy/hencoderplus/chapter50/Test50.java
+++ app/src/main/java/com/example/androidstudy/hencoderplus/chapter50/Test50.java
@@ -21,10 +21,14 @@ public class Test50 {
     }
 
     /**
-     * 第二题不知道题目 看需求应该是要把某个函数改成这样
+     * 题目在第二页 不过跟我猜的一样
      * 2. 放宽参数限制，改为填任何类型的参数都行，只要参数item的类型和参数list的List<>尖括号里的类型一致就可以
      */
-    <E> void someMethod1(E item, List<E> list) {
+//    public void merge(Object item, List<Object> list) {
+//        list.add(item);
+//    }
 
+    <E> void merge(E item, List<E> list) {
+        list.add(item);
     }
 }
