1418d1a598910b1468ff3f5d8fda25e76cbe8e10
朱家钰 <zhujiayu@36kr.com>
2022-07-13 10:44:29 +0800

    处理英文自动换行问题的扩展函数

---
+++ app/src/main/java/com/example/androidstudy/widget/Widget.kt
@@ -0,0 +1,34 @@
+package com.example.androidstudy.widget
+
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * @author zjy
+ * @date 2022/7/13 10:31 上午
+ * @description
+ */
+
+// 处理英文自动换行问题 在Textview.post{}中调用 获取处理好的文字后再设置文字
+fun TextView.toNonBreakingString(text: String?): String {
+    if (text == null) return ""
+    val container = parent as? ViewGroup ?: return text
+    val lineWidth = (container.width - container.paddingStart - container.paddingEnd).toFloat()
+    val maxCharsInOneLine = paint.breakText(text, 0, text.length, true, lineWidth, null)
+    if (maxCharsInOneLine == 0) return text
+    val sb = StringBuilder()
+    var currentLine = 1
+    var end = 0
+    for (i in 0..text.count() step maxCharsInOneLine) {
+        end = currentLine * maxCharsInOneLine
+        if (end > text.length) end = text.length
+        sb.append(text.subSequence(i, end))
+        sb.append("\n")
+        currentLine = currentLine.inc()
+    }
+    if (end < text.length) {
+        val remainingChars = text.length - end
+        sb.append(text.takeLast(remainingChars))
+    }
+    return sb.toString()
+}
