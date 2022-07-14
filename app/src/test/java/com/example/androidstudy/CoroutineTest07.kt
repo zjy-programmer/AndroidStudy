13eff57303f3d9bfe9dc5722abb2b22743178de0
zjy <zjy_programmer@163.com>
2022-07-14 22:00:01 +0800

    协程操作符学习

--- app/src/test/java/com/example/androidstudy/CoroutineTest07.kt
+++ app/src/test/java/com/example/androidstudy/CoroutineTest07.kt
@@ -1,11 +1,118 @@
 package com.example.androidstudy
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 /**
+ * ProjectName: AndroidStudy
+ * Package: com.example.androidstudy
+ * ClassName: CoroutineTest07
+ * CreateDate: 2022/6/25 12:00 下午
+ * Author: zjy
+ * Description:
+ */
+class CoroutineTest07 {
+    suspend fun performRequest(request: Int): String {
+        delay(1000)
+        return "response $request"
+    }
+
+    @Test
+    fun `test transform flow operator`() = runBlocking {
+        // map 传一个进来发一个出去
+//        (1..3).asFlow()
+//            .map { performRequest(it) }
+//            .collect { println(it) }
+
+        // transform 传一个进来可以发射多个出去
+        (1..3).asFlow()
+            .transform {
+                emit("Making request $it")
+                emit(performRequest(it))
+            }
+            .collect { println(it) }
+    }
+
+    fun numbers() = flow<Int> {
+        try {
+            emit(1)
+            emit(2)
+            println("This line will not execute")
+            emit(3)
+        } finally {
+            println("Finally in numbers")
+        }
+    }
+
+    // 只取两个
+    @Test
+    fun `test limit length operator`() = runBlocking {
+        numbers().take(2).collect { println(it) }
+    }
+
+    @Test
+    fun `test terminal operator`() = runBlocking {
+        val sum = (1..5).asFlow()
+            .map { it * it }
+            .reduce { a, b -> a + b }
+        println(sum)
+    }
+
+    @Test
+    fun `test zip`() = runBlocking {
+        val nums = (1..3).asFlow()
+        val strs = flowOf("One", "Two", "Three")
+        nums.zip(strs) { a, b ->
+            "$a -> $b"
+        }.collect { println(it) }
+    }
+
+    @Test
+    fun `test zip2`() = runBlocking {
+        val nums = (1..3).asFlow().onEach { delay(300) }
+        val strs = flowOf("One", "Two", "Three").onEach { delay(400) }
+        val startTime = System.currentTimeMillis()
+        nums.zip(strs) { a, b ->
+            "$a -> $b"
+        }.collect { println("$it at ${System.currentTimeMillis() - startTime} ms from start") }
+    }
+
+    fun requestFlow(i: Int) = flow<String> {
+        emit("$i: First")
+        delay(500)
+        emit("$i: Second")
+    }
+
+    @Test
+    fun `test flatMapConcat`() = runBlocking {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+//            .map { requestFlow(it) } //Flow<Flow<String>>
+            .flatMapConcat { requestFlow(it) }
+            .collect { println("$it at ${System.currentTimeMillis() - startTime} ms from start") }
+    }
+
+    @Test
+    fun `test flatMapMerge`() = runBlocking {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+            .flatMapMerge { requestFlow(it) }
+            .collect { println("$it at ${System.currentTimeMillis() - startTime} ms from start") }
+    }
+
+    @Test
+    fun `test flatMapLatest`() = runBlocking {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+            .flatMapLatest { requestFlow(it) }
+            .collect { println("$it at ${System.currentTimeMillis() - startTime} ms from start") }
+    }
  * @author zjy
  * @date 2022/7/4 6:31 下午
  * @description
